b579371f6e7bd088f85c0bb65b8bc12c45dea84f
Aishah999 <aishaah@code.edu.az>
2024-10-10 19:55:25 +0400

    sdsd

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace conditions1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //#region
+        //for(int i=1;i<100;i++)
+        //{
+        //if(i%15==0)
+        //{
+        //Console.WriteLine(i);
+        //}
+        // }
+        //#endregion
+
+        //#region
+
+        //var ortalama = 68;
+     
+        //if (ortalama < 65)
+        //{
+            //Console.WriteLine("Kesildin");
+        //}
+
+        //else if (ortalama >= 65 & ortalama < 85)
+        //{
+            //Console.WriteLine("Adi Diplom");
+        //}
+        //else if(ortalama>=85 & ortalama<95)
+        //{
+            //Console.WriteLine("Seref Diplomu");
+        //}
+        //else
+       //{
+            //Console.WriteLine("Yuksek Seref Diplomu");
+        //}
+
+
+
+        //#endregion
+    }
+}
